d3c6113125f5b4dfb9d35a5e1f5ae9c8ce1da42e
dongzhiheng <1428511549@qq.com>
2018-12-29 16:03:12 +0800

    first commit

---
+++ app/Http/Controllers/User/User.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Http\Controllers\User;
+    use Illuminate\Routing\Controller;
+    use App\Model\UserModel;
+    class User extends Controller {
+        public function test($id){
+           $user=UserModel::where(['id'=>$id])->first()->toArray();
+          echo "<pre>";
+          print_r($user);
+        }
+    }
+
+?>
