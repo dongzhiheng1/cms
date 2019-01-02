3c6d2464c2dad253e6893f5382df019d1d357b92
dongzhiheng <1428511549@qq.com>
2019-01-02 14:07:46 +0800

    测试

--- app/Http/Controllers/User/User.php
+++ app/Http/Controllers/User/User.php
@@ -8,6 +8,41 @@
           echo "<pre>";
           print_r($user);
         }
+        public function add(){
+            $data=[
+                'name'=>str_random(5),
+                'age'=>rand(1,100),
+                'create_time'=>time()
+            ];
+            $res=UserModel::insert($data);
+            var_dump($res);
+        }
+
+        public function update($id){
+            $data=[
+                'name'=>str_random(5),
+                'age'=>rand(1,100),
+            ];
+            $where=[
+                'id'=>$id
+            ];
+            $res=UserModel::where($where)->update($data);
+            var_dump($res);
+        }
+        public function delete($id){
+            $where=[
+                'id'=>$id
+            ];
+            $res=UserModel::where($where)->delete();
+            var_dump($res);
+        }
+        public function list(){
+            $list=UserModel::all();
+               $data=[
+                   'list'=>$list
+               ];
+             return view('User.index',$data);
+        }
     }
 
 ?>
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,9 @@ Route::get('/', function () {
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/user/{id}','User\user@test');
+//Route::get('/user/{id}','User\user@test');
+Route::get('/user/add','User\user@add');
+Route::get('/user/update/{id}','User\user@update');
+Route::get('/user/delete/{id}','User\user@delete');
+Route::get('/user/select','User\user@select');
+Route::get('/user/list','User\user@list');
